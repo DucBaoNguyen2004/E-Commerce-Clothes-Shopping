b43c0e0ed0b624c406098143b4f0400e28e358a3
DucBaoNguyen2004 <ducbao352@gmail.com>
2025-09-26 21:07:48 +0700

    xong item vs popular

---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Popular.css'
+import data_products from '../Assets/data'
+import { Item } from '../Item/Item'
+export const Popular = () => {
+  return (
+    <div className='popular'>
+        <h1>POPULAR IN WOMEN </h1>
+        <hr />
+        <div className="popular_item">
+            {data_products.map((item)=>{
+                return <Item key={item} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+            })}
+        </div>
+    </div>
+  )
+}
