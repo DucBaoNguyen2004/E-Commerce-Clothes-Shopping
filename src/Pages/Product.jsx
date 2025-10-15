7e03273fd49203973f57bac69dd3472f7ae481a7
DucBaoNguyen2004 <ducbao352@gmail.com>
2025-10-15 10:07:52 +0700

    xong breadcrum va productdisplay

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../Context/ShopContext'
 import { BreadCrum } from '../Components/BreadCrum/BreadCrum'
+import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay'
 import all_product from '../Components/Assets/all_product'
 export const Product = () => {
   const { all_product } = useContext(ShopContext)
@@ -13,6 +14,7 @@ console.log('params:', productId);
   return (
     <div>
       <BreadCrum product={product} />
+      <ProductDisplay product={product} />
     </div>
   )
 }
